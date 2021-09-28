4365c01423c343718cea93ce06185d33fdb0f372
Safwan Ali <safwan4872@gmail.com>
2021-09-28 14:59:38 +0600

    settingsmodified

--- backend/settings.py
+++ backend/settings.py
@@ -136,6 +136,8 @@ MEDIA_URL = "/media/"
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=3),
 }
+STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_DIRS = BASE_DIR/'static'
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
