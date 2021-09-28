ac6aa03799a2c2561126660b9dc37e453afef934
Safwan Ali <safwan4872@gmail.com>
2021-09-28 16:52:55 +0600

    urlspy update(test)

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls import url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns,static
+from django.conf.urls.static import static
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rest_framework_swagger.views import get_swagger_view
 from django.views.static import serve
 schema_view = get_swagger_view(title='Restaurant API')
@@ -33,3 +35,4 @@ urlpatterns = [
 ]
 urlpatterns+=staticfiles_urlpatterns()
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
